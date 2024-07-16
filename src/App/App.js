ff9f4c1eaa127133e8d4bde10f24c8cf6d05cfd1
Yeison Silva <yeisonsilvat06@gmail.com>
2024-07-15 22:46:53 -0500

    Se añadieron estilos en  TodoList

--- src/App/App.js
+++ src/App/App.js
@@ -38,10 +38,12 @@ function App() {
       <TodoCounter
         totalTodos={totalTodos}
         completedTodos={completedTodos}
+        loading={loading}
       />
       <TodoSearch
         searchValue={searchValue}
         setSearchValue={setSearchValue}
+        loading={loading}
       />
     </TodoHeader>
 
