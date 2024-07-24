14110d4bfa2cfd4e6a2e543e26bf2a006abfc9b3
Yeison Silva <yeisonsilvat06@gmail.com>
2024-07-23 23:02:34 -0500

    Estilos al componente CreateTodoButton

--- src/components/CreateTodoButton.jsx
+++ src/components/CreateTodoButton.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { PlusIcon } from "./PlusIcon";
 
 function CreateTodoButton({ setOpenModal }){
     return(
-    <button className="" 
+    <button className={`border rounded-full size-20 bg-indigo-200 border-indigo-500 text-indigo-500 font-bold`}
             onClick={ () => setOpenModal(state => !state)   
     }
-    
-    
-    >X</button>
+    >
+    <PlusIcon
+        className={`size-16`}
+    />
+    </button>
     );
 }
 
