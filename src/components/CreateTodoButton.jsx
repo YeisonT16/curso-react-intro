f80aede278b109ad15eae6bd058ab3b876cab04c
Yeison Silva <yeisonsilvat06@gmail.com>
2024-08-20 22:00:21 -0500

    Cambios en los estilos de los componentes CreateTodoButton y TodoItem

--- src/components/CreateTodoButton.jsx
+++ src/components/CreateTodoButton.jsx
@@ -4,12 +4,12 @@ import { PlusIcon } from "./PlusIcon";
 function CreateTodoButton({ setOpenModal, loading, openModal }){
 
     const buttonDisabled = loading ? 'hidden' : 'grid absolute'
-    const modalOpen = openModal ? 'transition ease-in-out delay-150 rotate-45' : ''
+    const modalOpen = openModal ? 'transition ease-in-out duration-300 rotate-45' : ''
     
     return(
     
-        <div className={`justify-items-stretch -top-6 bg-indigo-200 border rounded-full size-fit ${buttonDisabled}`}>
-            <button className={`bg-indigo-200 size-16 rounded-full border-indigo-500 text-indigo-500 font-normal  hover:bg-indigo-500 hover:opacity-50 hover:text-indigo-200 hover:scale-110 ${modalOpen}`}
+        <div className={`justify-items-stretch -top-6 bg-indigo-200 border rounded-full size-fit hover:scale-110 duration-300 ${buttonDisabled}`}>
+            <button className={`bg-indigo-200 size-16 rounded-full border-indigo-500 text-indigo-500 font-normal  hover:bg-indigo-500 hover:opacity-50 hover:text-indigo-200 duration-300 ${modalOpen}`}
             onClick={ () => setOpenModal(state => !state)}
 
             >
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,7 +6,7 @@ function TodoItem(props){
 
     const styleClickCheckIcon = props.completed ? 'bg-green-600 text-indigo-100' : 'cursor-pointer text-sky-600/85 bg-gray-700'
     const styleClickDeleteIcon = props.completed ? 'text-red-900 bg-stone-800' : 'text-red-600/85 bg-stone-700'
-    const styleCompleted = props.completed ? 'opacity-50 bg-gray-900 text-white' : 'text-indigo-200 bg-indigo-800'
+    const styleCompleted = props.completed ? 'opacity-50 bg-gray-900 text-white' : 'text-indigo-200 bg-indigo-800 hover:bg-indigo-500 hover:text-indigo-100 hover:scale-105 duration-300'
     return (
         <div className={`relative w-72 h-auto min-h-6 p-2 text-mdrounded-xl font-semibold border-4 mb-2 rounded-xl border-zinc-600 ${styleCompleted}`}>
             <span className={`absolute -left-2 -top-2 rounded-full hover:bg-green-600 hover:text-indigo-100 ${styleClickCheckIcon}`} onClick={props.onFinaliced}><CompleteIcon /></span>
